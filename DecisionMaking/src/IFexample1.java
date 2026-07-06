cce8c024f4b39c299156a06c566b3ff169e796ab
Devendra Verma <Devv70171@gamil.com>
2026-07-06 13:34:28 +0530

    hello

---
+++ DecisionMaking/src/IFexample1.java
@@ -0,0 +1,27 @@
+
+public class IFexample1 { 
+	public static void main(String[] args) {
+		
+ java.util.Scanner sc=new java.util.Scanner(System.in); 
+	 System.out.print("Enter your age:");
+	 int age=sc.nextInt(); 
+	 if (age>0 && age<=120 )
+	 System.out.println(" invalid");
+	 else if (age>0 && age <=4) {
+		 System.out.println("you  are Infane");
+         } else if(age>4&&age <=12) { 
+         System.out.println("you are child");
+         } else if(age>4 && age <=12) {
+            
+		 
+	 
+		 System.out.println("you are Yopung");
+     
+	 } else  if (age>50 && age<=80) {-
+	 System.out.println(" grand categories");
+ }
+ }  
+
+
+}
+  
